aba6a1558939be897b71e7c181f66f536924a7b0
Eric Shank <eric@everfi.com>
2015-05-08 13:36:44 -0400

    add course to main.css and update video task

--- tasks/video.js
+++ tasks/video.js
@@ -32,72 +32,31 @@ gulp.task('sync-videos',
 gulp.task('transcode-videos', function(cb) {
   var AWS = require('aws-sdk');
 
-  //var hls_64k_audio_preset_id = '1351620000001-200071';
-  var hls_180p_preset_id = '1426277042760-yxihot';
-  var hls_0400k_preset_id = '1351620000001-200050';
-  var hls_0600k_preset_id = '1351620000001-200040';
-  var hls_1000k_preset_id = '1351620000001-200030';
-  var hls_1500k_preset_id = '1351620000001-200020';
-  var hls_2000k_preset_id = '1351620000001-200010';
-  var hls_720p_preset = '1422479654864-puy87c';
-  var hls_1080p_preset = '1426276620673-7a129k';
-  var mp4_1080p_preset = "1351620000001-000001";
-  var mp4_720p_preset = "1351620000001-000010";
-  var mp4_480p_preset = "1351620000001-000020";
-  var webm_180_preset_id = "1423775283372-01mxh9";
-  var webm_360_preset_id = "1423775212416-rmpw8t";
-  var webm_480_preset_id = "1423775184722-zfn8w7";
-  var webm_720_preset_id = "1423775127554-vhnq30";
-  var webm_1080_preset_id = "1426276780574-zgemta";
-
-  var output_key = 'transcode-video';
-  var output_list = [
-    {
-      Key: 'hls_720p/' + output_key,
-      PresetId: hls_720p_preset,
-      SegmentDuration: '10'
+  var presets = {
+    hls: {
+      '0400k': '1351620000001-200050',
+      '0600k': '1351620000001-200040',
+      '1000k': '1351620000001-200030',
+      '1500k': '1351620000001-200020',
+      '2000k': '1351620000001-200010',
+      '180p': '1426277042760-yxihot',
+      '720p': '1422479654864-puy87c',
+      '1080p': '1426276620673-7a129k'
     },
-    {
-      Key: 'hls_180/' + output_key,
-      PresetId: hls_180p_preset_id,
-      SegmentDuration: '10'
+    mp4: {
+      '180p': '1351620000001-000061',
+      '360p': '1351620000001-000040',
+      '480p': "1351620000001-000020",
+      '720p': "1351620000001-000010",
+      '1080p': "1351620000001-000001"
     },
-    {
-      Key: 'hls_0400k/' + output_key,
-      PresetId: hls_0400k_preset_id,
-      SegmentDuration: '10'
-    },
-    {
-      Key: 'hls_0600k/' + output_key,
-      PresetId: hls_0600k_preset_id,
-      SegmentDuration: '10'
-    },
-    {
-      Key: 'hls_1000k/' + output_key,
-      PresetId: hls_1000k_preset_id,
-      SegmentDuration: '10'
-    },
-    {
-      Key: 'hls_1500k/' + output_key,
-      PresetId: hls_1500k_preset_id,
-      SegmentDuration: '10'
-    },
-    {
-      Key: 'hls_2000k/' + output_key,
-      PresetId: hls_2000k_preset_id,
-      SegmentDuration: '10'
-    },
-    {
-      Key: 'hls_1080p/' + output_key,
-      PresetId: hls_1080p_preset,
-      SegmentDuration: '10'
+    webm: {
+      '180p': "1423775283372-01mxh9",
+      '360p': "1423775212416-rmpw8t",
+      '480p': "1423775184722-zfn8w7",
+      '720p': "1423775127554-vhnq30",
+      '1080p': "1426276780574-zgemta"
     }
-  ];
-
-  var find_playlists = function () {
-    return output_list.map(function (o) {
-      return o.Key;
-    });
   };
 
   prompt.start();
@@ -118,6 +77,34 @@ gulp.task('transcode-videos', function(cb) {
     }
   };
   prompt.get(schema, function (err, responses) {
+    var videoKey = responses.video_name.toLowerCase().replace(/ |_/g, '-');
+
+    var output_list = [];
+    var playlist = [];
+    Object.keys(presets.hls).forEach(function(key){
+      var hlsId = 'hls_' + key + '/' + videoKey;
+      output_list.push({
+        Key: hlsId,
+        PresetId: presets.hls[key],
+        SegmentDuration: '10'
+      });
+      playlist.push(hlsId);
+    });
+    Object.keys(presets.mp4).forEach(function(key){
+      output_list.push({
+        Key: videoKey + '-' + key + '.mp4',
+        PresetId: presets.mp4[key]
+      });
+    });
+    Object.keys(presets.webm).forEach(function(key){
+      output_list.push({
+        Key: videoKey + '-' + key + '.webm',
+        PresetId: presets.webm[key]
+      });
+    });
+
+    output_list[0].ThumbnailPattern = videoKey + '-{count}';
+
     var params = {
       PipelineId: '1422479864529-p3ad8w',
       Input: {
@@ -125,51 +112,17 @@ gulp.task('transcode-videos', function(cb) {
         Resolution: 'auto',
         Container: 'mp4'
       },
-      OutputKeyPrefix: "videos/" + DEPLOY_CURRICULUM + "/" + responses.language + "/" + responses.video_name + "/v" + responses.version + "/",
+      OutputKeyPrefix: "curriculums/" + DEPLOY_CURRICULUM + "/media/videos/" + responses.language + "/" + videoKey + "/v" + responses.version + "/",
       Outputs: output_list,
       Playlists: [
         {
           Name: 'playlist',
           Format: 'HLSv3',
-          OutputKeys: find_playlists()
+          OutputKeys: playlist
         }
       ]
     };
-
-    output_list.push(
-      {
-        Key: output_key + '-1080.mp4',
-        PresetId: mp4_1080p_preset
-      },
-      {
-        Key: output_key + '-720.mp4',
-        PresetId: mp4_720p_preset
-      },
-      {
-        Key: output_key + '-480.mp4',
-        PresetId: mp4_480p_preset
-      },
-      {
-        Key: output_key + '-180.webm',
-        PresetId: webm_180_preset_id
-      },
-      {
-        Key: output_key + '-360p.webm',
-        PresetId: webm_360_preset_id
-      },
-      {
-        Key: output_key + '-480p.webm',
-        PresetId: webm_480_preset_id
-      },
-      {
-        Key: output_key + '-720p.webm',
-        PresetId: webm_720_preset_id
-      },
-      {
-        Key: output_key + '-1080p.webm',
-        PresetId: webm_1080_preset_id
-      }
-    );
+    console.log(output_list);
 
     var options = {
       region: 'us-east-1'
@@ -186,3 +139,4 @@ gulp.task('transcode-videos', function(cb) {
     });
   });
 });
+
