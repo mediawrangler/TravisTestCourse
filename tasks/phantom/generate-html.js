224beacf2ce3eb1e26ae187052519489e55caa43
Eric Shank <eric@everfi.com>
2015-06-08 15:48:29 -0400

    remove tasks folder

--- tasks/phantom/generate-html.js
+++
@@ -1,9 +0,0 @@
-var system = require('system');
-
-var page = require('webpage').create();
-page.open('__url__', function(status) {
-  window.setTimeout(function () {
-    console.log(page.content);
-    phantom.exit();
-  }, 200);
-});
